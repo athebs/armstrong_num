088d5960bb495fd0ba345dfbf3df307060868b7b
athebs <athena.s.tafur@gmail.com>
2024-02-04 10:09:14 -0600

    intial

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,12 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    armstrong_numbers = []
+    for number in range(numbers+1):
+       
+
+       
+    
+     
+    return armstrong_numbers
+result=find_armstrong_numbers(100)
+print(result)
